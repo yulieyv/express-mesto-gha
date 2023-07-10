4d1974071be64ad42bcba5d076d4a10f0ec9d0f3
Yulia Yamalieva <yulia.yamalieva@yandex.ru>
2023-07-10 22:41:50 +0300

    ПР14: test 14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,10 +4,12 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
-  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+  let token = '';
+  if (req.headers.cookie || req.headers.cookie.startsWith('jwt')) {
+    token = req.headers.cookie.replace('jwt=', '');
+  } else if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
     next(new UnauthorizedError('Токен отсутствует'));
-  }
-  const token = req.headers.authorization.replace('Bearer ', '');
+  } else token = req.headers.authorization.replace('Bearer ', '');
 
   let payload;
 
