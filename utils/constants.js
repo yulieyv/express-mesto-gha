c7c16c5b3f96454b7b622f32474881dec29c4053
Yulia Yamalieva <yulia.yamalieva@yandex.ru>
2023-07-16 14:12:34 +0300

    ПР14: ревью 1

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 const OK_STATUS = 200;
 const CREATED_STATUS = 201;
-const regexAvatar = /^https?:\/\/(?:[a-z0-9\\-]+\.)+[a-z]{2,6}(?:\/[^/#?]+)+\.(?:jpe?g|gif|png|bmp|webp)$/im;
+const regexAvatar = /^https?:\/\/(?:[a-z0-9\\-]+\.)+[a-z]{2,6}(?:\/[^/#?]+)+\.(?:jpe?g|gif|png|webp|bmp)$/im;
 const regexLink = /^https?:\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\\/~+#-]*[\w@?^=%&\\/~+#-])/im;
 
 module.exports = {
