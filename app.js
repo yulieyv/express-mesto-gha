d7689b3c74c380afdf73548f6dd70c542889e7c1
Yulia Yamalieva <yulia.yamalieva@yandex.ru>
2023-07-24 19:31:40 +0300

    ПР14: logger added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const errorHandler = require('./middlewares/errorHandler');
 const limiter = require('./middlewares/rateLimiter');
 const { PORT, DB_URL } = require('./utils/constants');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 
 const app = express();
@@ -16,8 +17,10 @@ mongoose.connect(DB_URL);
 app.use(helmet());
 app.use(express.json());
 app.use(limiter);
+app.use(requestLogger);
 app.use(cookieParser());
 app.use(router);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
