417623396b07efb8ba353174594c0155b85fabd5
Yulia Yamalieva <yulia.yamalieva@yandex.ru>
2023-07-26 22:01:31 +0300

    ПР14: версия без cookies 2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,9 +5,10 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(req.headers);
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Неправильные почта или пароль'));
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
